64df02cefe4334f5533369276e65fd5eba67afc2
Zanie Blue <contact@zanie.dev>
2026-08-19 10:44:44 -0500

    Require hashes for build dependencies

--- crates/uv-cli/src/lib.rs
+++ crates/uv-cli/src/lib.rs
@@ -2015,6 +2015,10 @@ pub struct PipSyncArgs {
     #[command(flatten)]
     pub hash_checking: HashCheckingArgs,
 
+    /// Require hashes for all dependencies installed into isolated build environments.
+    #[arg(long, env = EnvVars::UV_REQUIRE_BUILD_HASHES, value_parser = clap::builder::BoolishValueParser::new())]
+    pub require_build_hashes: bool,
+
     /// The Python interpreter into which packages should be installed.
     ///
     /// By default, syncing requires a virtual environment. A path to an alternative Python can be
@@ -2361,6 +2365,10 @@ pub struct PipInstallArgs {
 
     #[command(flatten)]
     pub hash_checking: HashCheckingArgs,
+
+    /// Require hashes for all dependencies installed into isolated build environments.
+    #[arg(long, env = EnvVars::UV_REQUIRE_BUILD_HASHES, value_parser = clap::builder::BoolishValueParser::new())]
+    pub require_build_hashes: bool,
 
     /// The Python interpreter into which packages should be installed.
     ///
